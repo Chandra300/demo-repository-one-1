c6746c3e09c9bfc212798c12f5aaedc14d97b21c
Chandra300 <24B11DS140@adityauniversity.in>
2025-10-15 08:18:34 +0530

    Add files via upload

---
+++ divison of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("enter the values");
+	scanf("%d%d",num1,num2);
+	float divison;
+	divison=num1/num2;
+	printf("the divison of %d and %d is %f",num1,num2,divison);
+	return 0;
+}
